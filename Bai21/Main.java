82a0f419d74d0be5fa0ed91c8a60bce4dd5a64b9
1anhphanle <anhphanle8818@gmail.com>
2024-05-26 09:39:25 +0700

    add Bai 21

---
+++ Bai21/Main.java
@@ -0,0 +1,36 @@
+package huongdoituong.Bai21;
+import huongdoituong.Bai21.HocSinh;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine(); // Consume the remaining newline
+
+        List<HocSinh> danhSachHocSinh = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            String hoTen = sc.nextLine();
+            double[] diemMonHoc = new double[10];
+            String[] diemStr = sc.nextLine().split(" ");
+            for (int j = 0; j < diemMonHoc.length; j++) {
+                diemMonHoc[j] = Double.parseDouble(diemStr[j]);
+            }
+
+            danhSachHocSinh.add(new HocSinh(hoTen, diemMonHoc));
+        }
+
+        // Sắp xếp danh sách học sinh theo điểm trung bình giảm dần
+        danhSachHocSinh.sort(Comparator.comparingDouble(HocSinh::tinhDiemTrungBinh).reversed()
+                .thenComparing(HocSinh::getMaHS));
+
+        // In ra danh sách học sinh đã được sắp xếp
+        for (HocSinh hocSinh : danhSachHocSinh) {
+            System.out.println(hocSinh);
+        }
+    }
+}
